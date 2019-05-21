6ea605d0e63912ff49de81aced06dd8a9a7bdfee
Rituj Kumar <rituj.kumar@tesco.com>
2019-05-21 20:39:05 +0530

    Adding changes

--- src/utils/apiUrls.js
+++ src/utils/apiUrls.js
@@ -2,7 +2,7 @@ import { url } from "./siteConf";
 export const baseUrl = url;
 
 export const loginUrl = "/users/permission?email=$email&password=$password";
-export const createUser = baseUrl + "/user/createUser";
+export const createUser = baseUrl + "/users/createUser";
 
 export const bookUrl = "/books/$bookTitle";
 export const videoUrl = "/videos/$videoTitle";
